d44e7b96bacb30857283db4ddd690b44a30282da
Alexander Kubczak <alexander.kubczak@gmail.com>
2019-08-21 11:09:26 +0200

    creating a hotpink navbar

--- routing-app/src/App.js
+++ routing-app/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
+import Nav from './nav.js';
 
 function App() {
   return (
     <div className="App">
-   
+    <Nav/>   
     </div>
   );
+
 }
 
 export default App;
---
+++ routing-app/src/nav.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './nav.css';
+
+const Nav = () => {
+	return(
+		<ul className = "navBar">
+			<li>Home</li>
+			<li>News</li>
+			<li>Contact</li>
+			<li>About</li>
+		</ul>
+
+		);
+
+};
+
+export default Nav;
