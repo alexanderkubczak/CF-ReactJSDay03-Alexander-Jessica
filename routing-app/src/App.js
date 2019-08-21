aefb9bb606d4a980de492caaced95e5c8b1c1ab5
Alexander Kubczak <alexander.kubczak@gmail.com>
2019-08-21 11:59:12 +0200

    creating an article and sidebar

--- routing-app/src/App.js
+++ routing-app/src/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import './App.css';
 import Nav from './nav.js';
-import Header from './Header.js'
+import Header from './Header.js';
+import Sidebar from './sidebar.js';
+import Content from './content.js';
 
 function App() {
   return (
     <div className="App">
     <Header/>
-    <Nav/>   
+    <Nav/>
+    <Sidebar/>
+    <Content/>
       
     </div>
   );
---
+++ routing-app/src/sidebar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './sidebar.css';
+
+const Sidebar = () => {
+	return(
+		<ul className = "sideBar">
+		<li>This</li>
+		<li>is</li>
+		<li>a</li>
+		<li>sidebar</li>
+		</ul>
+		);
+};
+export default Sidebar;
