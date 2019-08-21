6bd7c2340cf3d7722db7d661b061af81e0eb0680
JessicaStoimaier <jessica.stoimaier@gmx.at>
2019-08-21 15:36:48 +0200

    component content

--- routing-app/src/ContactComponent.js
+++ routing-app/src/ContactComponent.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import './contact.css'
+import './contact.css';
+import logo from './contact.jpg';
 
 const ContactComponent = () => {
  return (
     <div className = "contactstyle">
- 		
+ 		<img alt="logo" src={logo}/>
    </div> 
    )
 }
