1ea22c8a902b0d4d45023913db5854defd53cec2
JessicaStoimaier <jessica.stoimaier@gmx.at>
2019-08-21 13:00:23 +0200

    create NavLink

--- routing-app/src/App.js
+++ routing-app/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Route, Switch, Redirect, NavLink } from 'react-router-dom';
 import Nav from './nav.js';
 import Header from './Header.js'
 
@@ -7,7 +8,7 @@ function App() {
   return (
     <div className="App">
     <Header/>
-    <Nav/>   
+    <Nav/>  
       
     </div>
   );
--- routing-app/src/nav.js
+++ routing-app/src/nav.js
@@ -1,17 +1,34 @@
 import React from 'react';
 import './nav.css';
+import { BrowserRouter as Router, Route, Switch, Redirect, NavLink } from 'react-router-dom';
+import HomeComponent from './HomeComponent.js';
+import NewsComponent from './NewsComponent.js';
+import AboutComponent from './AboutComponent.js';
+import ContactComponent from './ContactComponent.js';
 
 const Nav = () => {
 	return(
-		<ul className = "navBar">
-			<li>Home</li>
-			<li>News</li>
-			<li>Contact</li>
-			<li>About</li>
-		</ul>
+		<Router>
+			<ul className = "navBar">
+				<li><NavLink to="/" exact activeClassName="active">Home</NavLink></li>
+				<li><NavLink to="/news" exact activeClassName="active">News</NavLink></li>
+				<li><NavLink to="/about" exact activeClassName="active">Contact</NavLink></li>
+				<li><NavLink to="/contact" exact activeClassName="active">About</NavLink></li>
+			</ul>
+
+			<main>
+				<Switch>
+					<Route path="/" exact component={HomeComponent} />
+					<Route path="/news" exact component={NewsComponent} />
+					<Route path="/about" exact component={AboutComponent} />
+					<Route path="/contact" exact component={ContactComponent} />
+					<Redirect to="/"/>
+				</Switch>
+			</main>
+		</Router>
 
 		);
 
-};
+}
 
 export default Nav;
